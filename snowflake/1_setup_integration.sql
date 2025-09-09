0cdb504da5b3ea4c6e7a2f51c11ddf7525fffecb
Harshitha Yalama <97040380+harshitha-y@users.noreply.github.com>
2025-09-09 12:31:39 -0400

    Updated comments

--- snowflake/1_setup_integration.sql
+++ snowflake/1_setup_integration.sql
@@ -1,10 +1,10 @@
--- Step 1: Create a database and warehouse if they don't exist
+--warehouse and DB creation
 CREATE WAREHOUSE IF NOT EXISTS my_wh;
 CREATE DATABASE IF NOT EXISTS music_analysis_db;
 USE music_analysis_db.public;
 
--- Step 2: Create a Storage Integration to securely connect to your AWS account
--- Replace 'YOUR_IAM_ROLE_ARN' and 'your-bucket-name' with your actual values
+--Creating a Storage Integration to securely connect to AWS account
+
 CREATE OR REPLACE STORAGE INTEGRATION s3_integration
   TYPE = EXTERNAL_STAGE
   STORAGE_PROVIDER = S3
@@ -12,15 +12,16 @@ CREATE OR REPLACE STORAGE INTEGRATION s3_integration
   STORAGE_AWS_ROLE_ARN = 'arn:aws:iam::ACCOUNT_ID:role/YOUR_IAM_ROLE_NAME' --<-- UPDATE THIS
   STORAGE_ALLOWED_LOCATIONS = ('s3://your-bucket-name/processed/'); --<-- UPDATE THIS
 
--- Step 3: IMPORTANT - Retrieve Snowflake's credentials to grant access in AWS
--- Run this command and follow the instructions in 'docs/snowflake_s3_integration.md'
+-- Retrieve Snowflake's credentials to grant access in AWS
+-- Run this command and get the ARN, add it to IAM trust policy
+--Snowflake won’t have permission to read your bucket without this
 DESC INTEGRATION s3_integration;
 
--- Step 4: Create a File Format for Parquet files
+-- Creating a File Format for Parquet files
 CREATE OR REPLACE FILE FORMAT my_parquet_format
   TYPE = PARQUET;
 
--- Step 5: Create an External Stage that points to your processed data
+--Creating an External Stage that points to processed data
 CREATE OR REPLACE STAGE my_s3_processed_stage
   STORAGE_INTEGRATION = s3_integration
   URL = 's3://your-bucket-name/processed/' --<-- UPDATE THIS
